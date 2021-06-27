00325b33beaf74e650e548bb5bdf70aee738e5d3
Frans Lennard Sipayung <84688195+mrfrans93@users.noreply.github.com>
2021-06-27 22:56:10 +1000

    add node core module file system
    to add node core module file system, accept input name and phone number and save it into .json file

--- coba.js
+++ coba.js
@@ -2,5 +2,26 @@ function panggilNama (nama){
     return `Hi, my name is ${nama}`;
 }
 
+//Readline
+const readLine = require('readLine');
+const rl = readLine.Interface({
+input:process.stdin,
+output:process.stdout,
+});
 
-module.exports = panggilNama;
+rl.question('Input your name : ', (nama) => {
+    rl.question('Input your phone number : ', (noHP) => {
+    const contact = {nama, noHP};
+    const file = fs.readFileSync('contacts.json', 'utf-8');
+    const contacts = JSON.parse(file);
+    contacts.push(contact);
+        
+    fs.writeFileSync('contacts.json', JSON.stringify(contacts));
+        
+    console.log('Thank you to input your data');
+        
+    rl.close();
+    });
+});
+
+//module.exports = panggilNama;
